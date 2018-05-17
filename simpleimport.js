7393f35f4504c047207d6dc12fdffbd7019d1b31
Shunki Takami <tkmnet@users.noreply.github.com>
2018-05-17 16:24:23 +0900

    Update simpleimport.js

--- simpleimport.js
+++ simpleimport.js
@@ -1,4 +1,3 @@
 var tkmlibSICreateHttpRequest=function(){if(window.ActiveXObject){try{return new ActiveXObject("Msxml2.XMLHTTP");}catch(e){try{return new ActiveXObject("Microsoft.XMLHTTP");}catch(e2){return null;}}}else if(window.XMLHttpRequest){return new XMLHttpRequest();}else{return null;}}
-var simpleread=function(file){var httpoj=tkmlibSICreateHttpRequest();httpoj.open("GET",file,"false");httpoj.send(null);return httpoj.responseText;}
 var simpleget=function(file,afterfunc){var httpoj=tkmlibSICreateHttpRequest();httpoj.open("GET",file,"true");httpoj.onreadystatechange=function(){if(httpoj.readyState==4){if(afterfunc!=undefined){afterfunc(httpoj.responseText);}}};httpoj.send("");}
 var simpleimport=function(targetI,fileI,afterfuncI){simpleget(fileI,function(response){document.getElementById(targetI).innerHTML=response;if(afterfuncI!=undefined){afterfuncI();}})}
